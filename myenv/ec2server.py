3be3dbd173d533958b1fd17a99dc6a011f916daf
herrnazareno <naza_10_11@hotmail.com>
2019-12-27 16:03:54 -0300

    downloadtoserver

--- myenv/ec2server.py
+++ myenv/ec2server.py
@@ -1,16 +1,24 @@
 from flask import Flask, request, Response
+import boto3
 import json
-
+import botocore
 app = Flask(__name__)
-
-@app.route("/<bucket>/<algunpath>", methods=['POST'])
-def hello(bucket, algunpath):
+s3 = boto3.client('s3')
+@app.route("/<bucket>/<object>", methods=['POST'])
+def hello(bucket, object):
     data = request.get_data()
+    s3 = boto3.resource('s3')
+    try:
+        s3.Bucket(bucket).download_file(object, str(data))
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            print("The object does not exist.")
+    
     print('mensaje del cliente : {}'.format(data))
-    return Response(algunpath)
+    return format(data)
 
-@app.route("/", methods=['GET'])
-def hello_get():
-    return "asd"
+@app.route("/<bucket>/<algunpath>", methods=['GET'])
+def hello_get(bucket, algunpath):
+    return 'msj recibido'
 
 app.run(host = '0.0.0.0', port = 8080)
--- myenv/requestsprueba.py
+++ myenv/requestsprueba.py
@@ -1,5 +1,5 @@
 import requests, json
 
-response = requests.get("http://0.0.0.0:8080/")
-r = requests.post("http://0.0.0.0:8080/", 'holakease' )
+response = requests.get("http://0.0.0.0:8080/challengemeli/asdf.txt")
+r = requests.post("http://0.0.0.0:8080/challengemeli/asdf.txt",'nombre' )
 print(response.content)
